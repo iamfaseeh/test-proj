014301519be79b08b840ff4fc439caa86ce401c8
abusufyan-ghafoor <abusufyan.ghafoor44@gmail.com>
2023-11-30 13:10:08 +0500

    code changes in github action file

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,13 +6,13 @@ class Product < ApplicationRecord
   has_many :line_items, dependent: :destroy
 
   validates :title, :description, :price, presence: true
-  # validates :upc, uniqueness: true
+  validates :upc, uniqueness: true
   validates :price, numericality: { greater_than: 0.0 }
   validates :weight, numericality: { greater_than: 0.0 }, if: -> { weight.present? }
   validates :weight_unit, presence: true, if: -> { weight.present? }
   validates :weight_unit, inclusion: { in: ALLOWED_WEIGHT_UNITS.values }, if: -> { weight_unit.present? }
 
-  # before_save :prevent_upc_change
+  before_save :prevent_upc_change
   before_destroy :validate_deletion, prepend: true
 
   private
